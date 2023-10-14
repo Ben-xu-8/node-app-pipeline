91a60fb3f0f33597c2f200a8b9b9bc17e58825d3
Ben <xuben1996@gmail.com>
2023-10-14 18:26:17 -0400

    Testing Pipeline 1

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,10 +43,11 @@ resource "aws_security_group" "maingroup" {
             cidr_blocks = [ "0.0.0.0/0" ]
             description = ""
             from_port = 0
-            ipv6_cidr_blocks = [ ]
-            prefix_list_ids = [  ]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
             protocol = "-1"
-            security_groups = [ ]
+            security_groups = []
+            self = false
             to_port = 0
         } 
     ]
@@ -55,10 +56,10 @@ resource "aws_security_group" "maingroup" {
             cidr_blocks = [ "0.0.0.0/0" ]
             description = ""
             from_port = 22
-            ipv6_cidr_blocks = [ ]
-            prefix_list_ids = [  ]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
             protocol = "tcp"
-            security_groups = [ ]
+            security_groups = []
             self = false
             to_port = 22
         },
@@ -66,10 +67,10 @@ resource "aws_security_group" "maingroup" {
             cidr_blocks = [ "0.0.0.0/0" ]
             description = ""
             from_port = 80
-            ipv6_cidr_blocks = [ ]
-            prefix_list_ids = [  ]
+            ipv6_cidr_blocks = []
+            prefix_list_ids = []
             protocol = "tcp"
-            security_groups = [ ]
+            security_groups = []
             self = false
             to_port = 80
         }
