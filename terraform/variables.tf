0f0d88f20f1c7e299fbd7bf180d6bfddee8c62b5
Ben <xuben1996@gmail.com>
2023-10-14 16:49:43 -0400

    Added deploy yaml file for github actions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,17 @@
 variable "region" {
   default = "us-east-1"
 }
+
 variable "public_key" {
-  
+  type = string
 }
 
 variable "private_key" {
-  
+  type = string
+
 }
 
 variable "key_name" {
-  
+  type = string
+
 }
